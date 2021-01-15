91e2618db354deec1dc9e4758cfeab68631e611c
Simone-Ortu <simo.28.09o@gmail.com>
2021-01-15 14:23:54 +0100

    Fine

--- js/script.js
+++ js/script.js
@@ -6,10 +6,48 @@
 // inserendo nell’ordine: nome, cognome e età.
 
 var studente = {
-    nome : 'Mario',
-    cognome : 'Rossi',
-    eta : 25
+    nome: 'Mario',
+    cognome: 'Rossi',
+    eta: 25
 };
-for (var k in studente){
-    console.log(k + ': ' + studente[k] );
+for (var k in studente) {
+    console.log(k + ': ' + studente[k]);
 }
+
+var classe = [
+    {
+        nome: 'Mario',
+        cognome: 'Rossi',
+        eta: 35
+    },
+    {
+        nome: 'Marco',
+        cognome: 'Neri',
+        eta: 40
+    },
+    {
+        nome: 'Giuseppe',
+        cognome: 'Verdi',
+        eta: 30
+    },
+    {
+        nome: 'Luca',
+        cognome: 'Bianchi',
+        eta: 20
+    }
+];
+
+for (var i = 0; i < classe.length; i++) {
+    console.log(classe[i].cognome);
+    console.log(classe[i].nome);
+    
+    
+}
+
+var nomeInsert = prompt('Inserire nome');
+var cognomeInsert = prompt('Inserire cognome');
+var etaInsert = parseInt(prompt('Inserire etá;'));
+
+classe.push({nome : nomeInsert , cognome : cognomeInsert , eta : etaInsert});
+
+console.log(classe);
